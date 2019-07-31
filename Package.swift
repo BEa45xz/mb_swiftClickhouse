de1fd2dcea791593c87637623a443838242d2a6c
BEa45xz <49711174+BEa45xz@users.noreply.github.com>
2019-07-31 14:34:53 +0200

    Update Package.swift
    added target for package

--- Package.swift
+++ Package.swift
@@ -2,7 +2,9 @@ import PackageDescription
 
 let package = Package(
   name:         "SwiftClickHouse",
-  targets:      [],
+  targets:      [
+      .target(name: "SwiftClickHouse", dependencies: ["SwiftyJson", "BlueSocket"]),
+  ],
   dependencies: [
       .package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
       .package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1, minor: 0)
