6df7cb01f570be243bcb26153833b3e14bb3aafb
BEa45xz <49711174+BEa45xz@users.noreply.github.com>
2019-07-31 14:31:00 +0200

    Update Package.swift
    swift generate xcodeproj hangs because of timout finding dependencies

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
   name:         "SwiftClickHouse",
   targets:      [],
   dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
-      .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1, minor: 0)
+      .package(url: "https://github.com/IBM-Swift/SwiftyJSON", majorVersion: 17, minor: 0),
+      .package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 1, minor: 0)
   ]
 )
